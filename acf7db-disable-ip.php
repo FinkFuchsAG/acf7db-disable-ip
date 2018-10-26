c628e1b9ba3a4b3b67a559cfcc5b56539da84645
Manuel Dudda <manuel.dudda@gmail.com>
2018-10-26 14:41:15 +0200

    first commit

---
+++ acf7db-disable-ip.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package ACF7DB_Disable_IP
+ * @version 1.0
+ */
+/*
+Plugin Name: ACF/DB Disable IP
+Plugin URI: https://finkfuchs.de
+Description: This plugin disables the ip storage of the form subimssions.
+Author: Manuel Dudda
+Version: 1.0
+Author URI: https://duddaweb.de/
+Text Domain: acf7db-disable-ip
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_action( 'after_setup_theme', 'acf7db_disable_ip' );
+
+function acf7db_disable_ip() {
+	do_shortcode( '[cf7-db-display-ip]' );
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== ACF7DB Disable IP ===
+Contributors: Manuel Dudda
+Stable tag: 1.0
+Tested up to: 4.9
+Requires at least: 4.6
+
+This plugin disables the ip storage of the form subimssions.
+
+== Description ==
+
+Extension to https://wordpress.org/plugins/advanced-cf7-db/
